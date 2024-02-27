4dc608010359286b702083f727ab9b8e8605e7be
Harun3821 <harun4382183@gmail.com>
2024-02-27 20:53:55 +0600

    first commit Api post creat kora

---
+++ post.js
@@ -0,0 +1,56 @@
+// function loadingPost(){
+//     fetch('https://jsonplaceholder.typicode.com/users/1/posts')
+//     .then(res => res.json())
+//     .then(data => displayPost(data))
+// }
+
+
+// function displayPost(posts){
+// const postConiter = document.getElementById("post-coniter");
+//   for(const post of posts){
+//      const div = document.createElement("div");
+//       div.classList.add("post");
+//      div.innerHTML = `
+//      <h4>users-${post.userId}</h4>
+//      <h5>Post : ${post.title}</h5>
+//      <p>Post Description: ${post.body}</p>
+
+//      `
+//      postConiter.appendChild(div);
+     
+    
+//   }
+// }
+
+
+// loadingPost()
+
+
+
+function loadingPost(){
+    fetch('https://jsonplaceholder.typicode.com/users/1/posts')
+    .then(res => res.json())
+    .then(data => displayPost(data))
+}
+
+
+
+function displayPost(posts){
+   const postConiter = document.getElementById("post-coniter");
+   for(const post of posts){
+    const div = document.createElement("div");
+    div.classList.add("post");
+    div.innerHTML = `
+     <h2>tital-${post.userId}</h2>
+     <h3>userid:-${post.title}</h3>
+     <h4>allbody:-${post.body}</h4>
+    `
+    postConiter.appendChild(div);
+
+   }
+
+}
+
+
+
+loadingPost()
